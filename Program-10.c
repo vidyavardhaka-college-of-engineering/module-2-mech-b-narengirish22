1aef7a40cbb9fbdcafcc864ecfcdd5b8424b6b21
narengirish22 <20ME0022@vvce.ac.in>
2021-03-08 17:14:25 +0000

    done

--- Program-10.c
+++ Program-10.c
@@ -32,8 +32,8 @@ int main()
         }
     }
 
-    printf("The largest element of the array = %d", large);
-    printf("The smallest element of the array = %d", small);
+    printf("The largest and smallest elements are %d and %d", large,small);
+   
 
     return 0;
 }
